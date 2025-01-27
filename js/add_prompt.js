9be4e9da9339740693a0aed643b1e5a27a32b3d9
Krishnanand Mishra <110751379+krishnanandgithub@users.noreply.github.com>
2025-01-28 00:56:59 +0530

    Add files via upload

---
+++ js/add_prompt.js
@@ -0,0 +1,8 @@
+//WAP to add two nos
+var a=window.prompt("Enter first no.: ");
+window.alert(a);
+var b=window.prompt("Enter second no.: ");
+window.alert(b);
+var sum=parseInt(a)+parseInt(b);
+window.alert("sum="+sum);
+document.write("sum="+sum);
